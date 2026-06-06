51c06990e8f05fd5dfe0a2116fea3a8b57e72f77
Ibrahim-Scythe <ibrahims07@outlook.com>
2026-06-06 18:43:39 +0100

    Added eyes to snake and refactored code

--- js/snake.js
+++ js/snake.js
@@ -1,14 +1,36 @@
 const isMobile = window.innerWidth <= 768;
 
+// Draw Components
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const drawSquare = (pos, size) => {
+    ctx.fillRect(pos[0],pos[1], size, size);
+}
+
+const drawText = (text) => {
+    ctx.font = '48px "Audiowide" ';
+    ctx.textAlign = 'center';
+
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+    ctx.strokeText(text, boardSize/2, boardSize/2);
+
+    ctx.fillStyle = "white";
+    ctx.fillText(text, boardSize/2, boardSize/2);
+}
+
+
+// Grid components
 const boardSize = canvas.height;
 const tileCount = 20;
 const tileSize = boardSize/tileCount;
 
+
+// Points Counter Components
 const pointsDisplay = document.getElementById("points");
 let points = 0;
+
 
 class Snake {
     constructor() {
@@ -16,10 +38,7 @@ class Snake {
             [boardSize/4, boardSize/2 + tileSize],
             [boardSize/4 + tileSize, boardSize/2 + tileSize]
         ];
-    }
-
-    getPos(n) {
-        return this.positions[n];
+        this.orientation = "R";
     }
 
     getAllPos() {
@@ -50,8 +69,47 @@ class Snake {
         if (!grow) this.positions.shift();
     }
 
+    draw() {
+        ctx.fillStyle = "white";
+        for (let i = 0; i < snake.positions.length; i++) {
+            drawSquare(this.positions[i], tileSize);
+        }
+
+        this.drawEyes();
+    }
+
+    drawEyes() {
+        const headPos = this.getHeadPos();
+        const eyeSize = tileSize/5;
+
+        const top = headPos[1]+tileSize*0.2;
+        const bottom = headPos[1]+tileSize*0.6;
+        const left = headPos[0]+tileSize*0.2;
+        const right = headPos[0]+tileSize*0.6;
+
+        ctx.fillStyle = "black";
+        switch (this.orientation) {
+            case "U":
+                drawSquare([right, top], eyeSize);
+                drawSquare([left, top], eyeSize);
+                break;
+            case "D":
+                drawSquare([right, bottom], eyeSize);
+                drawSquare([left, bottom], eyeSize);
+                break;
+            case "L":
+                drawSquare([left, top], eyeSize);
+                drawSquare([left, bottom], eyeSize);
+                break;
+            case "R":
+                drawSquare([right, top], eyeSize);
+                drawSquare([right, bottom], eyeSize);
+                break;
+        }
+    }
+
     checkCollision() {
-        const headPos = this.getHeadPos()
+        const headPos = this.getHeadPos();
         for (let i = 0; i < this.positions.length - 1; i++) {
             let partPos = this.positions[i];
             if (headPos[0] === partPos[0] && headPos[1] === partPos[1]) {
@@ -62,11 +120,11 @@ class Snake {
         }
         return false;
     }
-
 }
 
 // Initialising snake
 const snake = new Snake();
+
 
 // Returns a random number between min and max inclusive
 const randomNumber = (min, max) => {
@@ -95,10 +153,19 @@ class Pellet {
         }
     }
 
+    draw() {
+        ctx.fillStyle = "red";
+        drawSquare(this.pos, tileSize);
+    }
+
     getPos() {
         return this.pos;
     }
 }
+
+// Initialising pellet
+const pellet = new Pellet();
+
 
 let vX = 0;
 let vY = 0;
@@ -106,55 +173,28 @@ let vY = 0;
 let nextvX = tileSize;
 let nextvY = 0;
 
-const pellet = new Pellet();
 
 // Loads font and draws start screen
 async function initialScreen() {
     await document.fonts.ready;
 
-    ctx.fillStyle = "white";
-    for (let i = 0; i < snake.getLength(); i++) {
-        let pos = snake.getPos(i);
-        ctx.fillRect(pos[0],pos[1], tileSize, tileSize);
-    }
+    snake.draw();
 
-    ctx.fillStyle = "red";
-    ctx.fillRect(pellet.pos[0], pellet.pos[1], tileSize, tileSize);
-
-    ctx.font = '48px "Audiowide" ';
-    ctx.textAlign = 'center';
+    pellet.draw();
 
     let startText = "Press Space to start"
     if (isMobile) {
         startText = "Double tap to start";
     }
 
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 3;
-    ctx.strokeText(startText, boardSize/2, boardSize/2);
-
-
-    ctx.fillStyle = "white";
-    ctx.fillText(startText, boardSize/2, boardSize/2);
+    drawText(startText);
 }
-
 initialScreen();
 
 let gameOver = false;
-
 const gameLoop = () => {
     if (gameOver) {
-        ctx.font = '48px "Audiowide" ';
-        ctx.textAlign = 'center';
-
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 3;
-        ctx.strokeText("Game Over", boardSize/2, boardSize/2);
-
-
-        ctx.fillStyle = "white";
-        ctx.fillText("Game Over", boardSize/2, boardSize/2);
-
+        drawText("Game Over");
         return;
     }
     
@@ -185,27 +225,17 @@ const gameLoop = () => {
         pointsDisplay.textContent = points;
     }
 
-    // Clear canvas
+    // Clear canvas and redraw
     ctx.clearRect(0,0,canvas.width, canvas.height);
+    snake.draw();
+    pellet.draw();
 
-    // Draw Snake
-    ctx.fillStyle = "white";
-
-    for (let i = 0; i < snake.getLength(); i++) {
-        let pos = snake.getPos(i);
-        ctx.fillRect(pos[0],pos[1], tileSize, tileSize);
-    }
-
-    // Draw Pellet
-    ctx.fillStyle = "red";
-    ctx.fillRect(pellet.pos[0], pellet.pos[1], tileSize, tileSize);
-
+    // Check for self collisions
     gameOver = snake.checkCollision();
 };
 
-// Pressing the Space key will start/stop the race
+// Starts and pauses game
 let intervalID = null;
-
 const startAndStop = () => {
     if (intervalID === null) {
         intervalID = setInterval(gameLoop, 100);
@@ -222,11 +252,13 @@ const toggleHelpSection = () => {
     if (helpSection.style.display === '') helpSection.style.display = "block";
     else if (helpSection.style.display === "block") helpSection.style.display = '';
 }
+
 const helpBtn = document.getElementById("helpBtn");
 helpBtn.addEventListener("click", toggleHelpSection);
 
 const xBtn = document.getElementById("xBtn");
 xBtn.addEventListener("click", toggleHelpSection);
+
 
 // Keyboard Controls
 document.addEventListener('keydown', function(event) {
@@ -237,6 +269,7 @@ document.addEventListener('keydown', function(event) {
             if (vY !== tileSize) {
                 nextvX = 0;
                 nextvY = -1*tileSize;
+                snake.orientation = "U";
             }
             break;
 
@@ -245,6 +278,7 @@ document.addEventListener('keydown', function(event) {
             if (vY !== -1*tileSize) { 
                 nextvX = 0;
                 nextvY = tileSize;
+                snake.orientation = "D";
             }
             break;
 
@@ -253,6 +287,7 @@ document.addEventListener('keydown', function(event) {
             if (vX !== tileSize) {
                 nextvX = -1*tileSize;
                 nextvY = 0;
+                snake.orientation = "L";
             }
             break;
 
@@ -261,6 +296,7 @@ document.addEventListener('keydown', function(event) {
             if (vX !== -1*tileSize) {
                 nextvX = tileSize;
                 nextvY = 0;
+                snake.orientation = "R";
             }
             break;
 
